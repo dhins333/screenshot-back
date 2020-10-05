2dccb76dcf1c686ad5d07cce7c283170129d3aaa
Dhinesh Kumar <dhins333@gmail.com>
2020-10-05 09:49:16 +0530

    screenshot page

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const Games = require('../models/games');
+const Images = require('../models/images');
 
 const router = new express.Router();
 
-router.get('/games',async (req,res) => {
+router.get('/api/games',async (req,res) => {
     try{
         const games = await Games.find({});
         res.status(200).send(JSON.stringify(games));
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+router.get('/api/screenshots/:id',async (req,res) => {
+    try{
+        const images = await Images.find({game:req.params.id}).select({url:1,_id:0});
+        res.status(200).send(JSON.stringify(images));
     }
     catch(e){
         console.log(e);
