f47ed2891b071285e5cbeaad593c6ca85c21941c
Dhinesh Kumar <dhins333@gmail.com>
2020-10-05 11:48:59 +0530

    prod build

---
+++ public/precache-manifest.616443470d35f9d84f8d7c50d8a59311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a521711dfde2b3850c5e4cf9281116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f5b1a0889a862e1fdb6",
+    "url": "/static/css/2.24bf1742.chunk.css"
+  },
+  {
+    "revision": "170a371ec42bbad1f941",
+    "url": "/static/css/main.628ca086.chunk.css"
+  },
+  {
+    "revision": "6f5b1a0889a862e1fdb6",
+    "url": "/static/js/2.bb7adb70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb7adb70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170a371ec42bbad1f941",
+    "url": "/static/js/main.73837be6.chunk.js"
+  },
+  {
+    "revision": "c5bb75da1d9b912b7fc2",
+    "url": "/static/js/runtime-main.0faae0f1.js"
+  }
+]);
