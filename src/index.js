a27d8771511d1dd9d8dc600546a4f2ec19a96e99
Dhinesh Kumar <dhins333@gmail.com>
2020-11-03 19:41:13 +0530

    Error Page

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.use(express.static(path.join(__dirname,'..','public')));
 app.use(adminRouter);
 app.use(apiRouter);
 
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'..','public','index.html'));
+})
+
 app.listen(process.env.PORT,() => {
     console.log('Server Started');
 });
